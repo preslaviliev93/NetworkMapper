d94dfbcce2e77f8dd86f94b1d8a8a070d303173e
preslaviliev93 <preslaviliev105@gmail.com>
2025-07-29 15:53:09 +0300

    Fixed command bug

--- commands_mapper.py
+++ commands_mapper.py
@@ -1,8 +1,8 @@
 cmd_map = {
     "Linux": {
-        "get_arps": "cat /proc/net/arp"
+        "get_arps": ["cat", "/proc/net/arp"]
     },
     "Windows": {
-        "get_arps": "arp -a"
+        "get_arps": ["arp", "-a"]
     }
 }
